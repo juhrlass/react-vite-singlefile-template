56cb7025508250653b6b36afb93759492abed963
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2023-12-12 00:16:00 +0100

    Update Bingo Game

--- src/components/StartPage.tsx
+++ src/components/StartPage.tsx
@@ -9,7 +9,7 @@ export const StartPage = () => {
         "relative py-4 text-black flex flex-col items-center justify-center h-screen w-screen overflow-hidden"
       )}
     >
-      <Link to={"winter"}>
+      <Link to={"winterPage"}>
         <button className="text-6xl font-bold p-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-3xl focus:shadow-outline hover:bg-indigo-800">
           Winter
         </button>

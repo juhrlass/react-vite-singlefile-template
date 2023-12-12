267fb8ae8c59b7a92f73b6473c1ebd994f9ad4f2
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2023-12-12 08:02:36 +0100

    Switch winter image to webp

--- src/components/ui/DefaultPageComponent.tsx
+++ src/components/ui/DefaultPageComponent.tsx
@@ -1,14 +1,25 @@
 import { PropsWithChildren } from "react"
 import { cn } from "@/lib/utils.ts"
+import { useNavigate } from "react-router-dom"
+import { XCircleIcon } from "@heroicons/react/24/outline"
 
 export const DefaultPageComponent = (props: PropsWithChildren) => {
+
+
+  const navigate = useNavigate();
+
   return (
     <div
       className={cn(
         "relative py-4 bg-black text-white flex flex-col items-center justify-start h-screen w-screen overflow-hidden"
       )}
     >
+      <div className={"absolute  right-3 top-3"}>
+        <XCircleIcon onClick={()=> navigate(-1)} className="h-16 w-16" />
+      </div>
+
       {props.children}
     </div>
   )
 }
+

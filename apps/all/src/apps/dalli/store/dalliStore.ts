cb776a2bf60944d701b41cff8e03edf73667ae17
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2023-12-24 23:27:26 +0100

    Updates turbo

--- apps/all/src/apps/dalli/store/dalliStore.ts
+++ apps/all/src/apps/dalli/store/dalliStore.ts
@@ -4,6 +4,7 @@ interface DalliState {
   category: string
   tiles: number
   autoDrawDelay: number
+  playAudio:boolean
   setCategory: (newCategory: string) => void
   setTiles: (newTiles: number) => void
   setAutoDrawDelay: (autoDrawDelay: number) => void
@@ -12,6 +13,7 @@ export const useDalliStore = create<DalliState>((set) => ({
   category: "Tiere",
   tiles: 160,
   autoDrawDelay:1,
+  playAudio:true,
   setCategory: (newCategory: string) => set(() => ({ category: newCategory })),
   setTiles: (newTiles: number) => set(() => ({ tiles: newTiles })),
   setAutoDrawDelay: (newAutoDrawDelay: number) =>

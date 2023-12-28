f04d1f036442262cf89bfa4b801338ed5d038e92
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2023-12-28 22:02:08 +0100

    Updates turbo

--- apps/demo/src/pages/StartStartPage.tsx
+++ apps/demo/src/pages/StartStartPage.tsx
@@ -1,6 +1,5 @@
-import { DefaultPageComponent } from "../../../../packages/sharedui/components/ui/DefaultPageComponent.tsx"
-import { MenuCard } from "../../../../packages/sharedui/components/ui/MenuCard.tsx"
-import { MenuGrid } from "../../../../packages/sharedui/components/ui/MenuGrid.tsx"
+import { DefaultPageComponent, MenuCard, MenuGrid } from "@manifold/sharedui"
+
 
 export const StartStartPage = () => {
   return (
